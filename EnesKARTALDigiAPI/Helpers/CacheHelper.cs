e446243ac336a41d65db33c18904ba08b3be290e
EnesKartal <eneskartal117@gmail.com>
2020-12-02 21:30:17 +0300

    Logging, Cache, Routing, Json, JWT Token Bearer Authentication eklendi. Veritabanı bağlantısı ve model yapısı yapılandırıldı. Cache Helper eklendi.

---
+++ EnesKARTALDigiAPI/Helpers/CacheHelper.cs
@@ -0,0 +1,42 @@
+﻿using EnesKARTALDigiAPI.Data.Models;
+using EnesKARTALDigiAPI.Data.Repositories.Infra;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnesKARTALDigiAPI.Helpers
+{
+    public interface ICacheManager
+    {
+        bool CheckUserExists(string email);
+        void RemoveCache(string key);
+    }
+
+    public class CacheManager : ICacheManager
+    {
+        readonly IMemoryCache memoryCache;
+        IUserRepository userRepository;
+        public CacheManager(IUserRepository userRepository, IMemoryCache memoryCache)
+        {
+            this.userRepository = userRepository;
+            this.memoryCache = memoryCache;
+        }
+
+        public bool CheckUserExists(string email)
+        {
+            memoryCache.TryGetValue("USERLIST", out List<User> items);
+            if (items == null)
+            {
+                items = userRepository.GetAll().ToList();
+                memoryCache.Set("USERLIST", items, TimeSpan.FromDays(1));
+            }
+            return items.Any(p => p.Email == email);
+        }
+
+        public void RemoveCache(string key)
+        {
+            memoryCache.Remove(key);
+        }
+    }
+}
--- EnesKARTALDigiAPI/Startup.cs
+++ EnesKARTALDigiAPI/Startup.cs
@@ -1,24 +1,106 @@
+using EnesKARTALDigiAPI.Data.Models;
+using EnesKARTALDigiAPI.Data.Repositories.Infra;
+using EnesKARTALDigiAPI.Data.Repositories.Repo;
+using EnesKARTALDigiAPI.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EnesKARTALDigiAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter((category, level) =>
+                        category == DbLoggerCategory.Database.Command.Name
+                        && level == LogLevel.Information)
+                    .AddConsole();
+            });
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc();
+
+            services.AddControllers();
+
+            services.AddDbContext<DigiBlogDBContext>(o => o.UseSqlServer(Configuration["Database:ConnectionString"])
+                .UseLoggerFactory(MyLoggerFactory)
+
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                        );
+
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IPostRepository, PostRepository>();
+            services.AddTransient<ICommentRepository, CommentRepository>();
+
+            services.AddTransient<ICacheManager, CacheManager>();
+
+            services.AddMemoryCache();
+
+            //AUTHENTICATION
+            var key = Encoding.ASCII.GetBytes("eneskartaldigiturk");
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var cacheHelper = context.HttpContext.RequestServices.GetRequiredService<ICacheManager>();
+                        if (!cacheHelper.CheckUserExists(context.Principal.Identity.Name))
+                            context.Fail("Unauthorized");
+
+                        return Task.CompletedTask;
+                    }
+                };
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                    options.JsonSerializerOptions.MaxDepth = 256;
+
+                }).AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,14 +108,18 @@ namespace EnesKARTALDigiAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action}/{id?}");
             });
         }
     }
